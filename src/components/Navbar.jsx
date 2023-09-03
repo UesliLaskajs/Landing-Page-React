fc9326b3856e1ff81a9409a07eed1d8b0eefe2ec
UesliLaskajs <uesli.laska@gmail.com>
2023-09-03 23:11:39 +0200

    late night update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,47 +4,64 @@ import { Button } from './Button';
 import './Navbar.css'; // Corrected import statement
 
 function Navbar() {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(false);
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(false);
 
-  const clickHandler = () => {
-    setClick(!click);
-  }
-
-  const mobileButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
+    const clickHandler = () => {
+        setClick(!click);
     }
-  }
 
-  useEffect(() => {
-    mobileButton();
-    window.addEventListener('resize', mobileButton);
-    return () => {
-      window.removeEventListener('resize', mobileButton);
+    const mobileButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
     }
-  }, []);
 
-  const mobileMenu = () => setClick(false);
+    useEffect(() => {
+        mobileButton();
+        window.addEventListener('resize', mobileButton);
+        return () => {
+            window.removeEventListener('resize', mobileButton);
+        }
+    }, []);
 
-  return (
-    <div className={click ? 'navbar active' : 'navbar'}>
-      <div className="navbar-container">
-        <Link to='/' className="navbar-logo" onClick={mobileMenu}>
-          TRVL <i className="fab fa-typo3"></i>
-        </Link>
-        <div className="menu-icon" onClick={clickHandler}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+    const mobileMenu = () => setClick(false);
+
+    return (
+        <div className={click ? 'navbar active' : 'navbar'}>
+            <div className="navbar-container">
+                <Link to='/' className="navbar-logo" onClick={mobileMenu}>
+                    TRVL <i className="fab fa-typo3"></i>
+                </Link>
+                <div className="menu-icon" onClick={clickHandler}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/home' className="nav-links" onClick={mobileMenu} >
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className="nav-links" onClick={mobileMenu} >
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/products' className="nav-links" onClick={mobileMenu} >
+                            Products
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='sign-up' className="nav-links-mobile" onClick={mobileMenu}>Sign up</Link>
+                    </li>
+                </ul>
+                {button && <Button buttonStyle="btn--outline">Sign up</Button>}
+            </div>
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          {/* Add your nav links here */}
-        </ul>
-        {button && <Button buttonStyle="btn--outline">Sign up</Button>}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Navbar;
